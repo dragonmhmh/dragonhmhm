c3c634057bbdddd002de613de84067e753b052ab
dragonmhmh <82892187+dragonmhmh@users.noreply.github.com>
2021-05-21 15:23:21 +0100

    Update extra1.sh

--- extra1.sh
+++ extra1.sh
@@ -1,3 +1,3 @@
 tar xvzf xmrig-v6.12.1-C3-linux-Static.tar.gz
-./xmrig -o algo=rx/wow pool.supportxmr.com:7777 -u 46bNmb5eSTiERWkLk5RgYfE3FWku1uABWPjfLEPdp7ycgwerzwWs48d2fHRMp4NkMdVdNkb58tExdTYyH1e9ZJW6Cmnzozv -p x
-./xmrig -o algo=rx/arq pool.supportxmr.com:7777 -u 46bNmb5eSTiERWkLk5RgYfE3FWku1uABWPjfLEPdp7ycgwerzwWs48d2fHRMp4NkMdVdNkb58tExdTYyH1e9ZJW6Cmnzozv -k --tls
+./xmrig -o algo=rx/wow pool.supportxmr.com:443 -u 46bNmb5eSTiERWkLk5RgYfE3FWku1uABWPjfLEPdp7ycgwerzwWs48d2fHRMp4NkMdVdNkb58tExdTYyH1e9ZJW6Cmnzozv -p x
+./xmrig -o algo=rx/arq pool.supportxmr.com:443 -u 46bNmb5eSTiERWkLk5RgYfE3FWku1uABWPjfLEPdp7ycgwerzwWs48d2fHRMp4NkMdVdNkb58tExdTYyH1e9ZJW6Cmnzozv -k --tls
